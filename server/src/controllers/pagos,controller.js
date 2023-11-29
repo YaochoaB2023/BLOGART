cf1312c0b5a9b5c6635ff363f2cf36a24e19bd58
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-11-29 02:44:23 -0500

    pasarela de pago funcional

--- server/src/controllers/pagos,controller.js
+++ server/src/controllers/pagos,controller.js
@@ -1,29 +1,40 @@
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { acces_token } from '../config.js';
+import Carrito from '../models/carrito.models.js';
 
 export const createOrder = async (req, res) => {
     try {
+        //obtener las obras de cada usuario
+        const productosCarrito = await Carrito.find({
+        }).lean()
+
+        // mapear en un array las obras del usuario 
+        const Items = productosCarrito.map(producto => ({
+            id: producto._id.toString(),
+            title: producto.nombre,
+            quantity: producto.cantidad,
+            unit_price: producto.precio,
+        }));
+    
+        console.log('todas las obras:', Items);
+    
+        // res.json({ items });
+        // res.json({ message: "No hay productos en el carrito" });
+
+
+
         const client = new MercadoPagoConfig({ accessToken: acces_token, options: { timeout: 5000 } });
 
         const preference = new Preference(client);
 
-        // Intenta crear la preferencia
+
         const result = await preference.create({
             body: {
-                items: [
-                    {
-                        id: '1056928547',
-                        title: 'la monalisa',
-                        quantity: 3,
-                        unit_price: 100
-                    }
-                ],
+                items: Items,
             }
         });
 
-        // Verifica si result.body está definido antes de acceder a init_point
         const initPoint = result.init_point;
-        // Responde al frontend con initPoint o null
         res.send({ initPoint });
     } catch (error) {
         console.error('Error al crear la orden de pago:', error);
