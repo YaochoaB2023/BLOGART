83f70a9f24ebf5e49d11f69e8f449e00b3f779ee
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-11-29 12:08:47 -0500

    correcion de pasarela de pagos

--- server/src/routes/payment.routes.js
+++ server/src/routes/payment.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createOrder } from '../controllers/pagos,controller.js';
+import { requiredAuth } from "../middlewares/tokenValidation.js";
 
 const router = Router()
 
-router.post('/create-order', createOrder)
+router.post('/create-order',requiredAuth, createOrder)
 
 export default router
